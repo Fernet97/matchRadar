6bd4b70158ca128a9245ac1de9dbd9c206d0bd00
Fernet97 <44033607+Fernet97@users.noreply.github.com>
2021-06-25 00:50:24 +0200

    Link with Map of the Match

--- Components/FilterMyMatch.js
+++ Components/FilterMyMatch.js
@@ -12,19 +12,72 @@ export default function FilterMyMatch() {
 
   var days = ['Sunday','Monday','Tuesday',
      'Wednesday','Thursday','Friday','Saturday'];
+
+ const italianDays = {
+      "Monday" : "lunedì" ,
+      "Tuesday" : "martedì" ,
+      "Wednesday" : "mercoledì" ,
+      "Thursday" : "giovedì" ,
+      "Friday" : "venerdì" ,
+      "Saturday" : "sabato" ,
+      "Sunday" : "domenica" ,
+     };
+
   const [daySelected, setNewDay] = useState("Monday"); // Di default
-
   let date = new Date();
-
  //Fissa lo stato al giorno di oggi alprimo rendering
   useEffect(() => {
       setNewDay(days[date.getDay()]);
   }, []);
 
+  //Fetch data mie partite fino ai 7 giorni successivi
+  const [match, updateMatch] = useState([]);
+  // Fetching from my API ..
+  useEffect(() => {
+    const fetchData = async () => {
+      // const res = await fetch(
+      //   'https://catfact.ninja/fact',
+      // );
+      // const json = await res.json();
+      //console.log(JSON.stringify(json.fact));
+      //updateMatch(json.fact); //.fact è un campo
+      listMatch =
+      {"Monday": {
+          "nameSport":"Calcio a 7",
+          "nameField": "Palestra Presente",
+          "location": "Sala Consilina",
+          "hour": "18:00 - 19:00",
+          "partecipanti": "9/10",
+        },
+      "Thursday": {
+          "nameSport":"Basket",
+          "nameField": "Da peppino",
+          "location": "Atena Lucana",
+          "hour": "20:00 - 21:00",
+          "partecipanti": "1/10",
+        },
+      "Friday": {
+          "nameSport":"Tennis",
+          "nameField": "Zi Pepp",
+          "location": "Padula",
+          "hour": "12:00 - 13:00",
+          "partecipanti": "1/4",
+        }
+      };
 
-  const FetchedData = { //simulare tutto il cambio render con dati finti presi da api
+      //console.log(listMatch.Friday.nameSport);
 
-  }
+      updateMatch(listMatch);
+
+    };
+    fetchData();
+  }, [updateMatch]);
+
+
+
+  const ThereIsMatchDaySelected = true; //simulare tutto il cambio render con dati finti presi da api
+
+
 
   function handleChangeDay(newDay) {
     setNewDay(newDay);
@@ -37,9 +90,15 @@ export default function FilterMyMatch() {
         <WeekList day = {daySelected} startFrom = {days[date.getDay()] ||"Monday"} onChange = {handleChangeDay} />
       </View>
       {/* Mappa del match */}
-      <View  style={{flex: 7, width: '100%',  alignItems: "center" }}>
-        <MatchViewer nameSport = "Calcio a 5" field = "Palestra Futura"
-                    location = "Polla"  hour = "19:00 - 20:00"  partecipanti = "7/10" />
+      <View  style={{flex: 7, width: '100%',  alignItems: "center" , justifyContent: "center"}}>
+        {match[daySelected] ?(
+        <MatchViewer nameSport = {match[daySelected].nameSport} field = {match[daySelected].nameField}
+                    location = {match[daySelected].location}  hour = {match[daySelected].hour}  partecipanti = {match[daySelected].partecipanti} />
+        ): (
+          <Text style={{fontSize: 25,  fontFamily: 'evolve', paddingHorizontal: "10%", textAlign: 'center'}}>
+            Nessun match in programma per {days[date.getDay()] == daySelected ? "oggi" : italianDays[daySelected]}.
+          </Text>
+        )}
       </View>
     </View>
   );
